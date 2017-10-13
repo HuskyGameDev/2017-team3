393752dce8e64bde56a17d94457a7e3bf9d92b62
Jace Lutey <jclutey@mtu.edu>
2017-10-13 17:07:17 -0400

    Updated clock and player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,12 +11,12 @@ public class Player : MonoBehaviour {
 
 
     //attributes
-    protected int strength;
-    protected int dexterity;
-    protected int constitution;
-    protected int wisdom;
-    protected int intelligence;
-    protected int charisma;
+    protected uint strength;
+    protected uint dexterity;
+    protected uint constitution;
+    protected uint wisdom;
+    protected uint intelligence;
+    protected uint charisma;
 
 
     // Use this for initialization
@@ -32,5 +32,45 @@ public class Player : MonoBehaviour {
     void Die()
     {
         //if the character's stats result in death, do that stuff here
+    }
+
+    void StressMod(int amount)
+    {
+        //positive amount to increase stress, negative to decrease
+        stress = stress + amount;
+
+        //write something to tell the HUD that our stress level needs to be updated
+
+
+    }
+
+    void HomeworkMod(int amount)
+    {
+        //positive amount to increase homework, negative to decrease
+        homework = homework + amount;
+
+        //write something to tell the HUD that our homework level needs to be updated
+
+
+    }
+
+    void ExhaustionMod(int amount)
+    {
+        //positive amount to increase exhaustion, negative to decrease
+        exhaustion = exhaustion + amount;
+
+        //write something to tell the HUD that our exhaustion level needs to be updated
+
+
+    }
+
+    void MoneyMod(int amount)
+    {
+        //positive amount to increase homework, negative to decrease
+        money = money + amount;
+
+        //write something to tell the HUD that our money level needs to be updated
+
+
     }
 }
