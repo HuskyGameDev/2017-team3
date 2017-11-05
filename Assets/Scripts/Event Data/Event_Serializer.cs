7dbee804999d5374329b4f87f3fd4ac8ad1c59bb
Evan Overweg <ejoverwe@mtu.edu>
2017-11-05 18:24:16 -0500

    Switching to binary serialization

---
+++ Assets/Scripts/Event Data/Event_Serializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class Event_Serializer : MonoBehaviour {
+    public void Main(string[] args)
+    {
+           
+        Event e = new Event(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, null, "", 0, null, "", false, false);
+        XmlSerializer x = new XmlSerializer(e.GetType());
+        x.Serialize(stream, e);
+    }
+}
