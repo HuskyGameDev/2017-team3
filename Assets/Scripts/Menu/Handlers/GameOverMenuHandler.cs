306a9e7e8c1425d87e14d978123f0bdb4f6267fc
Daniel <djsaltzm@mtu.edu>
2017-12-03 15:47:00 -0500

    Work on game over menu

--- Assets/Scripts/Menu/Handlers/GameOverMenuHandler.cs
+++ Assets/Scripts/Menu/Handlers/GameOverMenuHandler.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverMenuHandler : MonoBehaviour {
     Clock clock;
     Player player;
-	// Use this for initialization
-	void Start () {
+    public Text results;
+    // Use this for initialization
+    void Start () {
         //get reference to the clock and player objects
         clock = (Clock)GameObject.FindGameObjectWithTag("Clock").GetComponent<Clock>();
         player = (Player)GameObject.FindGameObjectWithTag("PlayerStats").GetComponent<Player>();
         //TODO display if the player was successful or not as well as end stats
-        UnlockRewards 
+        UnlockRewards unlocking = new UnlockRewards();
+        Unlocked data = new Unlocked();
+        unlocking.unlock(player, clock, data);
+        results.text = "Results for " + player.name + ":\nStress: " + player.stress.ToString()
+            + "/100\nHomework: " + player.homework.ToString()
+            + "/200\nMoney: $" + player.money.ToString()
+            + "\nStrength: " + player.strength.ToString()
+            + "/100\nDexterity: " + player.dexterity.ToString()
+            + "/100\nConstitution: "+player.constitution.ToString()
+            + "/100\nWisdom: " +player.wisdom.ToString()
+            + "/100\nIntelligence: "+player.intelligence.ToString()
+            + "/100\nCharisma: "+player.charisma.ToString()
+            + "/100\nFriends: ";
     }
 	
 	// Update is called once per frame
