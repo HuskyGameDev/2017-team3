bc99d15955e48b49947c26f4a851af64600bd275
Daniel <djsaltzm@mtu.edu>
2017-12-07 18:44:47 -0500

    Implemented saving and loading data to the Player Prefs

---
+++ Assets/Scripts/Clock+player/Saved Data/XML_Save.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.IO;
+
+
+public class XML_Save{
+    private const string ADDRESS = "savedData";
+    protected XmlSerializer serialize = new XmlSerializer(typeof(Unlocked));
+    public XML_Save (Unlocked u)
+    {
+        TextWriter text = new StringWriter();
+        serialize.Serialize(text, u);
+        Debug.Log(text);
+        PlayerPrefs.SetString(ADDRESS, text.ToString());
+        PlayerPrefs.Save();
+    }
+}
