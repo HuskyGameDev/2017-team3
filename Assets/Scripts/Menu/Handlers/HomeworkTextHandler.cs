16961499160c28e4d67387109f7e91183a52781e
Trevor Good <tdgood@users.noreply.github.com>
2018-03-24 18:47:18 -0400

    Addition of GUI bars
    GUI bars to display stats instead of just plain text

---
+++ Assets/Scripts/Menu/Handlers/HomeworkTextHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HomeworkTextHandler : MonoBehaviour {
+
+    public Text txt;
+    Player player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("PlayerStats").GetComponent<Player>();
+        txt.text = player.homework.ToString() + "/200";
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        txt.text = player.homework.ToString() + "/200";
+    }
+}
---
+++ Assets/Scripts/Menu/Handlers/StressBarHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StressBarHandler : MonoBehaviour {
+
+    Player player;
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("PlayerStats").GetComponent<Player>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.localScale = new Vector3(((float)player.stress / 100), 1, 1);
+    }
+}
