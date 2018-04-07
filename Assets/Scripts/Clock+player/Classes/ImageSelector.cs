ba6b2e68b1a2b3bf66cc63b1ad0f454c5df31b24
Jace Lutey <jclutey@mtu.edu>
2018-04-07 19:46:24 -0400

    Start of Image Selector stuff

---
+++ Assets/Scripts/Clock+player/Classes/ImageSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ImageSelector : MonoBehaviour {
+    public GameObject obj;
+    Image img;
+
+	// Use this for initialization
+	void Start () {
+        img = obj.GetComponent<Image>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
